0d648c3f67625755931e0d4efbd03f01ac4896af
Jack Thomson <jackabt@amazon.com>
2026-02-06 16:08:39 +0000

    Add balloon perf

--- .buildkite/pipeline_perf.py
+++ .buildkite/pipeline_perf.py
@@ -90,6 +90,11 @@ perf_test = {
         "tests": "integration_tests/performance/test_hotplug_memory.py",
         "devtool_opts": "-c 1-10 -m 0",
     },
+    "balloon": {
+        "label": "balloon",
+        "tests": "integration_tests/performance/test_balloon.py",
+        "devtool_opts": "-c 1-10 -m 0",
+    },
 }
 
 REVISION_A = os.environ.get("REVISION_A")
